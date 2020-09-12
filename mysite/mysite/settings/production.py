c96d929acb9c9075585742bedfe2e0b1bf7f6aa8
e <e@estebantapia.com>
2020-09-12 22:19:24 +0900

    change to prod environment

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -1,6 +1,16 @@
 from .base import *
 
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'zh2v*fe5c&2ofpd=5t2tf@i$t%pkxc0(g*(bch@(myao*+(^y@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 try:
     from .local import *
