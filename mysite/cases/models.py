3c7fc04329435fea83b1b87ef25e3b684e1c4a10
E <e@estebantapia.com>
2020-10-20 15:12:18 +0900

    added search page

--- mysite/cases/models.py
+++ mysite/cases/models.py
@@ -215,3 +215,30 @@ class BlogCategory(models.Model):
 
 '''
 
+"""Search Page models."""
+from django.conf import settings  # 1
+from wagtail.core.models import Page  # 2
+from algoliasearch import algoliasearch  # 3
+
+
+#  4
+class SearchPage(Page):
+    """A search page class."""
+
+    template = "search_page.html"
+
+    class Meta:
+        verbose_name = "Search Page"
+        verbose_name_plural = "Search Pages"
+
+    def get_context(self, request):
+        """Algolia Searching. This view is used for page requests, not typing autocomplete."""
+        context = super().get_context(request)
+        query_string = request.GET.get("q") or False
+        if query_string:
+            client = algoliasearch.Client(
+                settings.ALGOLIA.get("6WI6WCU8N4"), settings.ALGOLIA.get("1b64a191c9eae1f5030161d78da30600")
+            )
+            algolia_index = client.init_index("test_index")
+            context["results"] = algolia_index.search(query_string)
+        return context
