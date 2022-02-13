97215fde04d314e7429f0a7007cacdd8c87ef174
E <e@estebantapia.com>
2022-02-14 00:44:24 +0900

    added gtag

--- web2/gatsby-starter-blog/gatsby-config.js
+++ web2/gatsby-starter-blog/gatsby-config.js
@@ -65,6 +65,26 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-45397918-1", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        /*
+          gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        */        
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
@@ -82,6 +102,8 @@ module.exports = {
             },
           },
         ],
+
+        
         
         /*
          remarkPlugins: [
--- web2/gatsby-starter-blog/src/pages/index.js
+++ web2/gatsby-starter-blog/src/pages/index.js
@@ -33,19 +33,46 @@ const IndexPage = () => (
           International background
         </p>
         <p>
-          Educated in the UK and Spain. I have focused on learning wide and varied skills from graphic design, marketing and product management.  
+          Educated in the UK and Spain and now based in Asia. 
+          I have focused on learning wide and varied skills from graphic design, marketing and product management.  
         </p>
         <Link
           to="/about/"
+          className="btn btn-primary"
         >
+        About E
         </Link>
         
       </Col>
+
+
     </Row>
     <Row>
       <Col className="py-5" lg="4" md="6">
       <h2>
-        Experience
+        Projects
+      </h2>
+        <p className="lead">
+          Brand design and strategy
+        </p>
+        <p>
+          I have redesigned and created various brands. By working on their core values we put them in a path that allows them and their audience to grow. 
+        </p>
+
+        <Link
+          to="/projects/"
+          className="btn btn-primary"
+        >
+        Branding
+        </Link>
+
+      </Col>
+    </Row>
+       
+    <Row>
+      <Col className="py-5" lg="4" md="6">
+      <h2>
+        Work trajectory
       </h2>
         <p className="lead">
           From concept to launch
@@ -54,9 +81,16 @@ const IndexPage = () => (
           I've market researched, designed and launched products in the IT and Food industries. I've also exported, promoted in international events and designed brands.
         </p>
 
+        <Link
+          to="/projects/"
+          className="btn btn-primary"
+        >
+        Experience
+        </Link>
+
       </Col>
     </Row>
-       
+
     </Container> 
     
 
